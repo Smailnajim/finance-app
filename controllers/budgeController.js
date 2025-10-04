c2ee0f54c84ece0694dcb0ee3bfb44abd94a700a
smailnajim <smailnajim2003@gmail.com>
2025-10-04 10:47:33 +0200

    insert into any budge by userId, category

--- controllers/budgeController.js
+++ controllers/budgeController.js
@@ -3,3 +3,26 @@ const models = require('./../models');
 exports.create = (req, res) => {
     
 }
+
+exports.inserIntoBudge = async (req, res)=>{
+    // req.session.user.id  bach nchof ga3 budge li 3ndo
+    // req.name bach nhded ina whda fihom 
+    // req.mony chal ghthet
+
+    try{
+        const budge = await models.Budge.findOne({
+            where: {userId: 1},
+            include: [{
+                model: models.Category,
+                as: 'category',
+                where: {name: 'wallet'},
+                attributes: []
+            }] 
+        });
+        budge.monthly = budge.monthly ? budge.monthly + mony : mony;
+        return;
+    }catch(error){
+        console.log('error->>>', error, '---<<<');
+    }
+    
+}
